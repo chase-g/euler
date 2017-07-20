c52078e4cd7edd411faeec113316f5219589a31a
chase-g <chasegorland@gmail.com>
2017-07-20 17:59:53 -0400

    Update euler1.py

--- euler1.py
+++ euler1.py
@@ -1,4 +1,5 @@
 #Project Euler problem 1
+#"Find the sum of all the multiples of 3 or 5 below 1000"
 import time
 start = time.time()
 print(sum([i for i in range(1,1000) if (i % 3 == 0 or i % 5 == 0)]))
