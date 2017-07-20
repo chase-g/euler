4f9a83030d61b5ab163291e7d838fbe5837ccbcd
chase-g <chasegorland@gmail.com>
2017-07-20 10:29:10 -0400

    Update euler5.py

--- euler5.py
+++ euler5.py
@@ -3,9 +3,9 @@ import time
 start = time.time()
 def smallestFactor(start):
     #list comprehension filters out numbers not divisible by 20-11
-    options = [i for i in range(start, start + 1000000, 20) if i % 20 == 0 and i % 19 == 0 \
-               and i % 18 == 0 and i % 17 == 0 and i % 16 == 0 and i % 15 == 0 and i % 14 == 0 \
-               and i % 13 == 0 and i % 12 == 0 and i % 11 == 0]
+    options = [i for i in range(start, start + 1000000, 20) if i % 11 == 0 and i % 13 == 0 \
+               and i % 14 == 0 and i % 16 == 0 and i % 17 == 0 and i % 18 == 0 and i % 19 == 0 \
+               and i % 20 == 0]
     #returns if found
     if len(options) > 0:
         print(options[0])
