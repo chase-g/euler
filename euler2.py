3216612490a25c3221e3011f768f3f2ebb90dedb
chase-g <chasegorland@gmail.com>
2017-07-20 18:01:28 -0400

    Update euler2.py

--- euler2.py
+++ euler2.py
@@ -1,4 +1,6 @@
 #Project Euler problem 2
+#"By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
+#find the sum of the even-valued terms."
 import time
 start = time.time()
 nums = [1,2]
