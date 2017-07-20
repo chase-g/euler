2b745aa5e6ae1c3d10a1b5e6cae0632a29df0590
chase-g <chasegorland@gmail.com>
2017-07-19 20:17:27 -0400

    Add files via upload

---
+++ euler5.py
@@ -0,0 +1,18 @@
+#Project Euler problem 5
+import time
+start = time.time()
+def smallestFactor(start):
+    #list comprehension filters out numbers not divisible by 20-11
+    options = [i for i in range(start, start + 1000000, 20) if i % 20 == 0 and i % 19 == 0 \
+               and i % 18 == 0 and i % 17 == 0 and i % 16 == 0 and i % 15 == 0 and i % 14 == 0 \
+               and i % 13 == 0 and i % 12 == 0 and i % 11 == 0]
+    #returns if found
+    if len(options) > 0:
+        print(options[0])
+        return options[0]
+    #recursion with larger numbers if not found
+    else:
+        smallestFactor(start + 1000000)
+smallestFactor(2520)
+end = time.time()
+print("Speed: " + str(end - start))
