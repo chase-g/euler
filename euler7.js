22e9538e574822a95a1ecbeadb038656c03ed3c2
chase-g <chasegorland@gmail.com>
2017-07-25 20:14:23 -0400

    Update euler7.js

--- euler7.js
+++ euler7.js
@@ -3,42 +3,34 @@
 //Sieve of Eratosthenes
 var t0 = performance.now();
 //starting arrays
-var notPrime = [0,1];
-var prime = [];
-function sieve(first, num){
-  //loop over every number up to limit
-  for(var i = first; i < num + 1; i++){
-    //check whether i is already in notPrime array
-    if(notPrime.includes(i)){
-      //already in notPrime so do nothing
-    } else{
-//if not in notPrime:
-//if not already in prime, add to prime (avoid redundance on recursion)
-      if(!prime.includes(i)){
-      prime.push(i);
-      console.log(i);
-    }
-      //loop through multiples of i
-      for(var n = i + i; n < num + 105000; n += i){
-        //add multiples of i to notPrime array if not there
-        if(!notPrime.includes(n)){
-        notPrime.push(n);
-      }
-      }
-    }}}
-//Call recursively to reach prime #10,001
-function recur(begin, end){
-  if(prime.length < 10001){
-    //call sieve with parameters
-    sieve(begin, end);
-    //recur with next 100
-    recur(begin + 1000, end + 1000);
+var notPrime = new Array(150000).fill(false);
+notPrime[0] = true;
+notPrime[1] = true;
+var prime = []
+//loop over every number up to limit
+for(var i = 2; i < notPrime.length; i++){
+  //check whether i is already in notPrime array
+  if(notPrime[i] == true){
+    //already in notPrime so do nothing
   } else{
-    //output value when reached on recursive call
-    console.log("Answer: " + String(prime[10000]));
-    return prime[10000];
+//if prime:
+    //loop through multiples of i and make them true
+    for(var n = i + i; n < 150000; n += i){
+      //add multiples of i to notPrime array if not there
+      notPrime[n] = true;
+    }
   }
 }
-recur(0, 1000);
+for(var i = 0; i < notPrime.length; i++){
+  //check whether i is in the notPrime array
+  //if not prime, do nothing
+  if(notPrime[i] == true){
+  } else{
+//if prime, add to prime array
+    prime.push(i);
+    }
+  }
+  //print 10,001st prime number
+console.log("Answer: " + prime[10000]);
 var t1 = performance.now();
 console.log("Speed: " + String(t1 - t0) + " milliseconds");
