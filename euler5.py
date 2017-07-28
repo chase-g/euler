6c744dbf3e896fd0a34432655da223b2a2b21781
chase-g <chasegorland@gmail.com>
2017-07-28 17:22:47 -0400

    Update euler5.py

--- euler5.py
+++ euler5.py
@@ -1,4 +1,6 @@
 #Project Euler problem 5
+#"What is the smallest positive number that is
+#evenly divisible by all of the numbers from 1 to 20?"
 import time
 start = time.time()
 def smallestFactor(start):
