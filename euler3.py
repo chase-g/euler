05f5ef77ab6bf6a1427cc5c2f9ac6c78b866c46a
chase-g <chasegorland@gmail.com>
2017-07-28 12:16:58 -0400

    Update euler3.py

--- euler3.py
+++ euler3.py
@@ -1,14 +1,17 @@
 #Project Euler Problem 3
+#"What is the largest prime factor of the number 600851475143?"
 #recursive algorithm
 import time
 import math
 start = time.time()
 number = 600851475143
+#create list of numbers (odd only) which are divisors of number
 factors = [i for i in range(int((number ** .5) + 1), 2, -1) if number % i == 0]
 primeNums = []
 print(factors)
 def prime(num, factorList):
     ans = factorList[0]
+#base case, if number is divisible by the potential answer and it's prime
     if num % ans == 0 and ans in primeNums:
         print(ans)
         return ans
@@ -17,6 +20,7 @@ def prime(num, factorList):
         prime(num, factorList)
     else:
         prime(num, factorList[1:])
+#call prime using the list of factors of the number
 prime(number, factors)
 end = time.time()
 print("Speed: ")
