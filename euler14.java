dd0ac8ee8573db460b5899e9246dadc398df45cb
chase-g <chasegorland@gmail.com>
2017-08-03 20:45:25 -0600

    Update euler14.java

--- euler14.java
+++ euler14.java
@@ -12,22 +12,30 @@ public class euler14
 	public static int record = 0;
    public static void main(String[] args)
    {
-
+//start timer
 	final long startTime = System.currentTimeMillis();
+	//iterate over every number up to one million
 	   for(int i = 2; i < 1000000; i++) {
+	//set number to perform collatz sequence
 	   long collatz = i;
+	//counts number of steps in the collatz sequence
 	   int counter = 0;
+	//run sequence until reaching one
 	   while(collatz != 1) {
+//if even, perform operation and increment counter
 		   if(collatz % 2 == 0) {
 			  collatz = collatz / 2; 
 			  counter++;
+//if it reaches one and counter exceeds existing record, set and print new record
 			  if(counter > record && collatz == 1) {
 				   record = counter;
 				   System.out.println(i + ", current longest chain: " + record);
 			   }
+//if odd, perform operation and increment counter
 		   } else {
 			   collatz = (3 * collatz) + 1;
 			   counter++;
+//if it reaches one and counter exceeds existing record, set and print new record
 			   if(counter > record && collatz == 1) {
 				   record = counter;
 				   System.out.println(i + "--current longest chain: " + record);
@@ -35,6 +43,7 @@ public class euler14
 		   }
 	   }
     }
+//end timer and output speed
 final long endTime = System.currentTimeMillis();
 System.out.println("Speed in milliseconds: " + (endTime - startTime));
   }
