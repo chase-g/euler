fb97048fc5d6854c5c7af9a9560b13f0f290f44f
chase-g <chasegorland@gmail.com>
2017-08-05 09:50:11 -0600

    Update euler15.java

--- euler15.java
+++ euler15.java
@@ -3,7 +3,7 @@
  * "Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
  * How many such routes are there through a 20×20 grid?"
  * Answer can be found with binomial coefficient
- * @version August 3, 2017
+ * @version August 4, 2017
  * @author chase-g
  */
 import java.math.BigInteger;
