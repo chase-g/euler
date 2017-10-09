001edd0b6f9d596cfd8266b3c87a8afffa7f72d4
chase-g <chasegorland@gmail.com>
2017-10-08 22:15:37 -0400

    Update euler23.scala

--- euler23.scala
+++ euler23.scala
@@ -6,7 +6,8 @@
     val t0 = System.nanoTime()
     //function to find factors
     def factors(num: Int) = {
-        val factors = for(i <- 1 to (num - 1) if num % i == 0) yield { i }
+        val max = (num / 2) + 1
+        val factors = for(i <- 1 to max if num % i == 0) yield { i }
         factors
     }
     //function to find sums
@@ -19,21 +20,24 @@
       if(num <= 20161){
       val numFactors = factors(num)
       val factorSum = sumList(numFactors)
+      //if the number is abundant, prepend it to the running list of abundant numbers in the function call
       if(factorSum > num) { abundance(num + 1, num :: currentList) }
       else { abundance(num + 1, currentList) }
       } else return currentList
     }
     //all abundant numbers
-    val abundant = abundance(2, List())
+    val abundant = abundance(3, List())
     //function to create a list of numbers which cannot be formed as the sum of two abundant numbers
     def notSumAbundant(num: Int, currentList: List[Int]): List[Int] = {
       val abundantList = abundant.filter(_ < num)
       if(num <= 20161) { 
-        if(!abundantList.exists(x => abundantList.contains(num - x))) { notSumAbundant(num + 1, num :: currentList) }
-        else { notSumAbundant(num + 1, currentList) }
+        //if it cannot be made as the sum of abundant numbers, prepend it to the current list when calling on the next number
+        if(!abundantList.exists(x => abundantList.contains(num - x))) { 
+          notSumAbundant(num + 1, num :: currentList) 
+          } else { notSumAbundant(num + 1, currentList) }
      }  else return currentList
     }
-
+    //store the values and then sum them
     val answerList = notSumAbundant(1, List())
     val answer = answerList.reduceLeft(_ + _)
     val t1 = System.nanoTime()
