17e331e66b8412084948a2eb1ad08d14925297d9
chase-g <chasegorland@gmail.com>
2017-10-20 21:31:09 -0400

    Update euler30.java

--- euler30.java
+++ euler30.java
@@ -7,30 +7,30 @@
 public class euler30{
 	public static void main(String[] args) {
 		double[] selected;
-		selected = new double[1000000];
-		for(int i = 2; i < 1000000; i++) {
+		selected = new double[1000000];  // create array with capacity for all potential values 
+		for(int i = 2; i < 1000000; i++) {  // loop without including one
 			String iStr;
-			iStr = String.valueOf(i);
+			iStr = String.valueOf(i);  // create string of each number value and split into array of digits
 			String [] strArray = iStr.split("");
 			double[] expArray;
-			expArray = new double[100];
-			for(int n = 0; n < strArray.length; n++) {
-				int num = Integer.valueOf(strArray[n]);
-				double exp = Math.pow(num, 5);
-				expArray[n] = exp;
+			expArray = new double[10];
+			for(int n = 0; n < strArray.length; n++) {  // for each digit 
+				int num = Integer.valueOf(strArray[n]);  // get int value of the digit
+				double exp = Math.pow(num, 5);  // get x^5 for that digit
+				expArray[n] = exp;  // append to the expArray
 			}
 			double sum = 0;
-			for(int t = 0; t < expArray.length; t++) {
-				sum += expArray[t];
+			for(int t = 0; t < expArray.length; t++) { 
+				sum += expArray[t];  // iterate through each exArray value to generate their sum
 			}
-			if(sum == i) {
+			if(sum == i) {  // if the sum of digits^5 equals the original number, save it to the memo array
 				selected[i] = i;
 			}
 		}
 		int total = 0;
 		for(int e = 0; e < selected.length; e++) {
-			total += selected[e];
+			total += selected[e];  // sum the values in the array of selected numbers
     }
-		System.out.println(total);
+		System.out.println(total); 
   }
 }
