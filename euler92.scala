b439ee995ad88d5e3bdaadc4c7398c4509666e01
chase-g <chasegorland@gmail.com>
2017-12-19 23:29:07 -0500

    Update euler92.scala

--- euler92.scala
+++ euler92.scala
@@ -9,26 +9,28 @@
 object euler92 {
   def main(args: Array[String]): Unit = {
     val t0 = System.nanoTime()
+    
     val blank = new Array[Boolean](10000000)
     def chain(number: Int, links: List[Int] = List()): Unit = {
-      val next = number.toString.toList.map(_.toString).map(_.toInt).map(x => x * x).sum
-      if (next == 89) for (i <- number :: links) blank(i) = true
+      val next = number.toString.map(i => (i - '0') * (i - '0')).sum
+      if (next == 89) for (i <- number :: links; n <- i.toString.permutations.toList.map(_.toInt)) blank(n) = true
       else if (next == 1) ()
       else {
         chain(next, number :: links)
       }
     }
+
     def checkAll(num: Int, counter: Int = 0): Unit = {
-      if (num == 10000000) println("Answer: " + counter)
-      else if (blank(num) == true) checkAll(num + 1, counter + 1)
+      if (num == 1) println("Answer: " + counter)
+      else if (blank(num) == true) checkAll(num - 1, counter + 1)
       else {
         chain(num)
-        if (blank(num) == true) checkAll(num + 1, counter + 1)
-        else checkAll(num + 1, counter)
+        if (blank(num) == true) checkAll(num - 1, counter + 1)
+        else checkAll(num - 1, counter)
       }
     }
-    checkAll(1)
-
+    checkAll(9999999)
+    
     val t1 = System.nanoTime()
     println("Elapsed time: " + (t1 - t0) + " ns")
   }
