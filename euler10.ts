3d1726c6871609fd3438e08249c544bb2a38a6b0
chase-g <chasegorland@gmail.com>
2018-03-24 00:08:38 -0400

    Add files via upload

---
+++ euler10.ts
@@ -0,0 +1,26 @@
+//Project Euler Problem 10
+//"Find the sum of all the primes below two million."
+//Sieve of Eratosthenes
+function sieve(peak: number) {
+var notPrime: Array<boolean> = new Array(peak);
+notPrime[0] = true;
+notPrime[1] = true;
+var total = 0;
+//loop over every number up to limit
+for(var i = 2; i < notPrime.length; i++){
+  //check whether i is already in notPrime array
+  if(notPrime[i] == true){
+    //already in notPrime so do nothing
+  } else{
+//if prime:
+    total += i;
+    //loop through multiples of i and make them true
+    for(var n = i + i; n < peak; n += i){
+      //add multiples of i to notPrime array if not there
+      notPrime[n] = true;
+    }
+  }
+}
+console.log(total);
+}
+sieve(2000000);
