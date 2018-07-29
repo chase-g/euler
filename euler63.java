bad7c9ce0b9f1147f907973954917db134ebb561
chase-g <chasegorland@gmail.com>
2018-07-29 13:34:42 -0400

    Update euler63.java

--- euler63.java
+++ euler63.java
@@ -2,21 +2,20 @@ import java.math.BigInteger;
 
 class euler63 {
 	public static void main(String[] args) {
-      int runningCount = 0;
-      int currentPower = 1;
-      while(currentPower > 0) {
-    	  int numMatches = iteratePowers(currentPower);
-    	  if(numMatches == 0) {
-    		  currentPower = 0;
-    	  }
-    	  else {
-    		  runningCount += numMatches;
-    		  currentPower++;
-    	  }
-      }
-      System.out.println("answer: " + runningCount);
+		int runningCount = 0;
+		int currentPower = 1;
+		while (currentPower > 0) {
+			int numMatches = iteratePowers(currentPower);
+			if (numMatches == 0) {
+				currentPower = 0;
+			} else {
+				runningCount += numMatches;
+				currentPower++;
+			}
+		}
+		System.out.println("answer: " + runningCount);
 	}
-	
+
 	public static int iteratePowers(int power) {
 		BigInteger base = BigInteger.valueOf(1);
 		int counter = 0;
